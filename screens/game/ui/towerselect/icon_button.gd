a127f9059a8efc10897d0a38afdd96bb7e1e2a75
Drew Finstrom <dgf1979@users.noreply.github.com>
2026-02-24 11:37:59 -0800

    tower selection enable/disable
    -disable tower selection on level load
    -re-enable on game started

--- screens/game/ui/towerselect/icon_button.gd
+++ screens/game/ui/towerselect/icon_button.gd
@@ -5,6 +5,11 @@ var _tower_data: TowerData
 func setup(tower_data: TowerData) -> void:
 	_tower_data = tower_data
 	icon = tower_data.load_thumbnail_texture()
+	disabled = true
+	Event.game_start.connect(_on_game_start)
 
 func _on_pressed() -> void:
 	Event.tower_select.emit(_tower_data)
+	
+func _on_game_start() -> void:
+	disabled = false
